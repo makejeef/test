29fdddf0e6b4c126ed69fa5cbac4d645f7cc19d2
makejeef <makejeef@126.com>
2017-04-12 10:47:58 +0800

    remove cookie.py test1.py

--- scrapy_douban/cookie.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-import http.cookiejar
-cookie=http.cookiejar.CookieJar()
-handler=urllib.request.HTTPCookieProcessor(cookie)
-opener=urllib.request.build_opener(handler)
-response=opener.open('http://www.douban.com')
-for item in cookie:
-	print('Name='+item.name)
-	print('Value='+item.value)
--- scrapy_douban/test2.py
+++
@@ -1,8 +0,0 @@
-import http.cookiejar
-import urllib.request
-cookie=http.cookiejar.MozillaCookieJar()
-cookie.load('cookie.txt',ignore_discard=True,ignore_expires=True)
-req=urllib.request.Request('https://douban.com')
-opener=urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cookie))
-response=opener.open(req)
-print(response.read())
